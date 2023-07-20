9f5804419fb2fad4fa2f4323fa696cdc937ebf8f
msoyturk <msoyturk@gams.com>
2023-07-20 11:30:52 +0200

    add __all__ for from gamspy import * kinda statements

--- src/gamspy/__init__.py
+++ src/gamspy/__init__.py
@@ -5,5 +5,24 @@ from gamspy._model import Model, ModelStatus
 from gamspy._symbols import Alias, Set, Parameter, Variable, Equation
 from gamspy._algebra import Domain, Number, Sum, Product, Smax, Smin, Ord, Card
 
-_order = 0
+_order = 0  # Global order for newly generated symbols with no name
 __version__ = "0.0.1"
+
+__all__ = [
+    "Container",
+    "Model",
+    "ModelStatus",
+    "Alias",
+    "Set",
+    "Parameter",
+    "Variable",
+    "Equation",
+    "Domain",
+    "Number",
+    "Sum",
+    "Product",
+    "Smax",
+    "Smin",
+    "Ord",
+    "Card",
+]
