4b2246985d62ddc0bfbd6e309d2b4d930bab0e93
msoyturk <msoyturk@gams.com>
2023-07-27 15:07:03 +0200

    remove manifest.in

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Distribution
+import platform
+import os
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+def get_minigams_path():
+    user_os = platform.system().lower()
+    minigams_path = "minigams" + os.sep + user_os
+
+    if user_os == "darwin":
+        minigams_path += f"_{platform.machine()}"
+
+    minigams_path += os.sep + "*"
+    return minigams_path
+
+
+setup(
+    packages=["gamspy"],
+    package_dir={"gamspy": "src/gamspy"},
+    package_data={"gamspy": [get_minigams_path()]},
+    distclass=BinaryDistribution,
+)
