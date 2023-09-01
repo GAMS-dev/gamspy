a21a33d8c3253f385713702d312027f26d58f257
msoyturk <msoyturk@gams.com>
2023-09-01 11:12:32 +0200

    update abcs

--- src/gamspy/_symbols/symbol.py
+++ src/gamspy/_symbols/symbol.py
@@ -4,8 +4,8 @@ from abc import ABC, abstractmethod
 class Symbol(ABC):
     @abstractmethod
     def gamsRepr(self):
-        ...
+        """Representation of the symbol in GAMS"""
 
     @abstractmethod
     def getStatement(self):
-        ...
+        """Declaration string of the symbol in GAMS"""
