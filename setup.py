d91f9b39008e6ecf6e84fda51a0fb4702d7f6a85
Adam Christensen <achristensen@gams.com>
2023-09-29 14:52:38 -0400

    add in dependency to transfer

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ import platform
 import os
 
 
-
 setup(
     packages=["gamspy"],
     package_dir={"gamspy": "src/gamspy"},
-    install_requires=[
-        "gamsapi==45.0.0",
-        "gamspy_base==45.0.0"
-    ],
+    install_requires=["gamsapi[transfer]==45.0.0", "gamspy_base==45.0.0"],
 )
