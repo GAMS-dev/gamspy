3ecfd247c643864c9aa5db52318085fe5c6f2668
burak <busul@gams.com>
2023-10-04 11:29:54 +0200

    hardcode version for docs

--- src/gamspy/version.py
+++ src/gamspy/version.py
@@ -3,4 +3,8 @@ try:
 except ImportError:
     import importlib_metadata as metadata
 
-__version__ = metadata.version("gamspy")
+try:
+    __version__ = metadata.version("gamspy")
+except KeyError:
+    # Working with non-compiled version
+    __version__ = "0.9.0"
