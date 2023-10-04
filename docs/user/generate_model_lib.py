dd837058a4b22ff8e0878a54bc2cea889fe4a274
Justine Broihan <jbroihan@gams.com>
2023-10-04 17:43:51 +0200

    add a script

---
+++ docs/user/generate_model_lib.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+import urllib.request
+import urllib.error
+
+
+def open_url(request):
+  try:
+    return urllib.request.urlopen(request)
+  except urllib.error.HTTPError as e:
+    return e
+
+files = os.listdir("tests/integration/models")
+
+csv = {'Model': [], 'GAMSPy': [], 'GAMS': []}
+
+for f in files:
+    if 'py' in f:
+        name = f.split(".")[0]
+
+        file_str = f".. _{name}:\n\n{name}\n===========================================\n\n.. literalinclude:: ../../../tests/integration/models/{name}.py\n"
+        
+        links = [
+            f'https://www.gams.com/latest/gamslib_ml/libhtml/gamslib_{name}.html',
+            f'https://www.gams.com/latest/finlib_ml/libhtml/finlib_{name}.html',
+            f'https://www.gams.com/latest/noalib_ml/libhtml/noalib_{name}.html'
+            ]
+        
+        for link in links:
+            if open_url(link).status == 200:
+                # Table
+                csv['Model'].append(name)
+                csv['GAMSPy'].append(f':ref:`GAMSPy <{name}>`')
+                csv['GAMS'].append(f'`GAMS <{link}>`__')
+                # rst
+                with open(f"docs/examples/model_lib/{name}.rst", "w") as rst:
+                    rst.write(file_str)
+                break
+
+pd.DataFrame(csv).to_csv('docs/examples/model_lib/table.csv', index=False)
