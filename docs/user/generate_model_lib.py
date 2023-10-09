d94a13140ed29855ea43208b9e1eca08076620a3
Justine Broihan <jbroihan@gams.com>
2023-10-09 09:44:35 +0200

    add comments

--- docs/user/generate_model_lib.py
+++ docs/user/generate_model_lib.py
@@ -20,18 +20,14 @@ for f in files:
     if "py" in f:
         name = f.split(".")[0]
 
-        data = ""
-        matching_files = glob.glob(f"tests/integration/models/{name}*")
+    # Find data files
         for m in matching_files:
             if not 'py' in m:
                 data = m
 
-        file_str = (
+        # configure .rst file
             f":orphan:\n\n"
-            f".. _{name}:\n\n{name}\n===========================================\n\n.."
-            f" literalinclude:: ../../../tests/integration/models/{name}.py\n"
-        )
-
+        # model libraries to check
         links = [
             f"https://www.gams.com/latest/gamslib_ml/libhtml/gamslib_{name}.html",
             f"https://www.gams.com/latest/finlib_ml/libhtml/finlib_{name}.html",
@@ -39,6 +35,7 @@ for f in files:
             f"https://www.gams.com/latest/psoptlib_ml/libhtml/psoptlib_{name}.html"
         ]
 
+        # check in which lib the model is
         for link in links:
             found = False
             if open_url(link).status == 200:
@@ -47,12 +44,9 @@ for f in files:
                 csv["Model"].append(name)
                 csv["GAMSPy"].append(f":ref:`GAMSPy <{name}>`")
                 csv["GAMS"].append(f"`GAMS <{link}>`__")
-                csv["Data"].append(data)
-                # rst
-                with open(f"docs/examples/model_lib/{name}.rst", "w") as rst:
-                    rst.write(file_str)
+                # write rst
                 break
         if not found:
             print(f'{name} not found in lib')
 
-pd.DataFrame(csv).sort_values(by='Model', key=lambda col: col.str.lower()).to_csv("docs/examples/model_lib/table.csv", index=False)
+# write model lib table
