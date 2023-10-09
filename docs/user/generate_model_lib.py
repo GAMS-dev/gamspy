3f5e0437227e6bce14ee80a3c5a11745d26d5570
Justine Broihan <jbroihan@gams.com>
2023-10-09 10:19:21 +0200

    change title

--- docs/user/generate_model_lib.py
+++ docs/user/generate_model_lib.py
@@ -17,6 +17,7 @@ csv = {"Model": [], "GAMSPy": [], "GAMS": [], "Data": []}
 
 for f in files:
     name = f.stem
+    title = f.read_text().split('\n')[1]
 
     # Find data files
     data_file = ""
@@ -29,49 +30,50 @@ for f in files:
                 data_file = m
                 data_name = m.name
 
-        # configure .rst file
-        rst_head = (
-            f":orphan:\n\n"
-            f".. _{name}:\n\n{name}\n===========================================\n\n"
-        )
+    # configure .rst file
+    rst_head = (
+        f":orphan:\n\n"
+        f".. _{name}:\n\n{title}\n===========================================\n\n"
+        f":download:`{f.name} <{PurePosixPath('../../..', f)}>` "
+    )
 
-        rst_foot = f".. literalinclude:: {PurePosixPath(f'../../../tests/integration/models/{name}.py')}\n"
+    rst_foot = f"\n\n.. literalinclude:: {PurePosixPath(f'../../../tests/integration/models/{name}.py')}\n"
 
-        if data:
-                rst_data = (
-                f"Data |{data_name}|\n\n"
-                f".. |{data_name}| replace::\n"
-                f"   :download:`{data_name} <{PurePosixPath('../../..', data_file)}>`\n\n"
-            )
+    if data:
+        data_link = f":download:`{data_name} <{PurePosixPath('../..', data_file)}>`"
+        rst_data = (
+        f"|{data_name}|\n\n"
+        f".. |{data_name}| replace::\n"
+        f"   :download:`{data_name} <{PurePosixPath('../../..', data_file)}>`\n\n"
+    )
+        rst_str = rst_head + rst_data + rst_foot
+    else:
+        data_link = ""
+        rst_str = rst_head + rst_foot
 
-        if data:
-            rst_str = rst_head + rst_data + rst_foot
-        else:
-            rst_str = rst_head + rst_foot
+    # model libraries to check
+    links = [
+        f"https://www.gams.com/latest/gamslib_ml/libhtml/gamslib_{name}.html",
+        # f"https://www.gams.com/latest/finlib_ml/libhtml/finlib_{name}.html",
+        # f"https://www.gams.com/latest/noalib_ml/libhtml/noalib_{name}.html",
+        # f"https://www.gams.com/latest/psoptlib_ml/libhtml/psoptlib_{name}.html"
+    ]
 
-        # model libraries to check
-        links = [
-            f"https://www.gams.com/latest/gamslib_ml/libhtml/gamslib_{name}.html",
-            f"https://www.gams.com/latest/finlib_ml/libhtml/finlib_{name}.html",
-            f"https://www.gams.com/latest/noalib_ml/libhtml/noalib_{name}.html",
-            f"https://www.gams.com/latest/psoptlib_ml/libhtml/psoptlib_{name}.html"
-        ]
-
-        # check in which lib the model is
-        for link in links:
-            found = False
-            if open_url(link).status == 200:
-                found = True
-                # Table
-                csv["Model"].append(name)
-                csv["GAMSPy"].append(f":ref:`GAMSPy <{name}>`")
-                csv["GAMS"].append(f"`GAMS <{link}>`__")
-                csv["Data"].append(data_file)
-                # write rst
-                Path(f"docs/examples/model_lib/{name}.rst").write_text(rst_str)
-                break
-        if not found:
-            print(f"{name} not found in lib")
+    # check in which lib the model is
+    for link in links:
+        found = False
+        if open_url(link).status == 200:
+            found = True
+            # Table
+            csv["Model"].append(name)
+            csv["GAMSPy"].append(f":ref:`GAMSPy <{name}>`")
+            csv["GAMS"].append(f"`GAMS <{link}>`__")
+            csv["Data"].append(data_link)
+            # write rst
+            Path(f"docs/examples/model_lib/{name}.rst").write_text(rst_str)
+            break
+    if not found:
+        print(f"{name} not found in lib")
 
 # write model lib table
 pd.DataFrame(csv).sort_values(by="Model", key=lambda col: col.str.lower()).to_csv(
