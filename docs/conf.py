0d11590ab2a8e645418d90a08ab07ff67f9ba4a6
msoyturk <msoyturk@gams.com>
2023-10-20 16:28:48 +0200

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = "GAMSpy"
 copyright = "2023, GAMS Development Corporation"
 author = "GAMS"
-release = "0.9.0"
+release = "0.10.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
