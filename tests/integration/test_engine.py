be958460faee477b91d5d6b65565247cd97b22eb
msoyturk <msoyturk@gams.com>
2024-03-11 16:53:09 +0100

    update engine test

--- tests/integration/test_engine.py
+++ tests/integration/test_engine.py
@@ -427,8 +427,6 @@ class EngineSuite(unittest.TestCase):
         status, _, _ = client.job.get(token)
         while status != 10:
             status, _, _ = client.job.get(token)
-            output_buffer, _ = client.job.get_logs(token)
-            print(output_buffer)
 
         # /api/auth/logout -> post
         message = client.auth.logout()
