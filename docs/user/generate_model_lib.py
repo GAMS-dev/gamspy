0907d6299f47f8f6fcf4518adb423a3b03bf17db
msoyturk <msoyturk@gams.com>
2024-03-13 16:01:34 +0100

    update docs model library table

--- docs/user/generate_model_lib.py
+++ docs/user/generate_model_lib.py
@@ -4,13 +4,12 @@ from pathlib import Path
 from pathlib import PurePosixPath
 
 import pandas as pd
-import subprocess
-
 
 models = list(Path("tests/integration/models").glob("*.py"))
 
 csv = {
-    "Model": [],
+    "GAMSPy Model": [],
+    "GAMS Model": [],
     "Data": [],
     "Model Type": [],
     "License": [],
@@ -47,7 +46,7 @@ for model in models:
 
     # Model's GAMS source
     gams_source = model_info_dict.get("GAMSSOURCE", "")
-    gams_source = f"`GAMSSOURCE <{gams_source}>`__" if gams_source else ""
+    gams_source = f"`{name} <{gams_source}>`__" if gams_source else ""
 
     # Model's license type
     license_type = model_info_dict.get("LICENSETYPE")
@@ -64,7 +63,8 @@ for model in models:
     )
 
     ## APPENDING DATA TO .csv FILE
-    csv["Model"].append(f"{gamspy_source}   {gams_source}")
+    csv["GAMSPy Model"].append(gamspy_source)
+    csv["GAMS Model"].append(gams_source)
     csv["Data"].append(data_files_link)
     csv["License"].append(license_type)
     csv["Model Type"].append(model_type)
@@ -97,47 +97,5 @@ for model in models:
 
 
 pd.DataFrame(csv).sort_values(
-    by="Model", key=lambda col: col.str.lower()
+    by="GAMSPy Model", key=lambda col: col.str.lower()
 ).to_csv(Path("docs/examples/model_lib/table.csv"), index=False)
-
-
-# Go to docs directory for the remaining part
-directory = Path("docs").resolve()
-
-command = ["make", "html"]
-
-process = subprocess.Popen(
-    command,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    cwd=Path("docs").resolve(),
-)
-stdout, stderr = process.communicate()
-
-if process.returncode != 0:
-    print(f"An error occurred: {stderr.decode('utf-8')}")
-else:
-    print(stdout.decode("utf-8"))
-
-content = (
-    (directory / "_build" / "html" / "user" / "model_library.html")
-    .read_text()
-    .splitlines()
-)
-
-content_n = []
-
-for line in content:
-    if "GAMSSOURCE" in line:
-        line_n = line.replace(
-            "GAMSSOURCE",
-            "<img src='../_static/gams.svg' class='icon-link-table' alt='GAMS'/>",
-        )
-        content_n.append(line_n)
-
-    else:
-        content_n.append(line)
-
-(directory / "_build" / "html" / "user" / "model_library.html").write_text(
-    "\n".join(content_n)
-)
