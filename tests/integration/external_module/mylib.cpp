dc4682be40746be13930a5bbfe7c56a0dfa0b5f9
burak <busul@gams.com>
2024-08-11 15:23:17 +0200

    try adding external equation test

---
+++ tests/integration/external_module/mylib.cpp
@@ -0,0 +1,104 @@
+#define GE_EXPORT
+#include "geheader.h"
+#include <stdexcept>
+#include <iostream>
+#include <cassert>
+#include <stdlib.h>
+#include <memory>
+#include <string.h>
+#include <sstream>
+#include <cmath>
+
+ GE_API int GE_CALLCONV
+gefunc( int *icntr, double *x, double *f, double *d, msgcb_t msgcb)
+{
+    int  i, j, findex, dofnc, dodrv, neq, nvar, nz, rc=0;
+
+    if ( icntr[I_Mode] == DOINIT ) {
+        neq = 2; // We expect 2 external equations
+        nvar = 4; // 4 variables at total
+        nz = 4; // And 4 differentibles
+        if ( neq != icntr[I_Neq] ) {
+            GElog ( icntr, "--- Number of equations do not match");
+            rc = 2;
+        }
+        else if (nvar != icntr[I_Nvar]) {
+            GElog ( icntr, "--- Number of variables do not match");
+            rc = 2;
+        }
+        else if (nz != icntr[I_Nz]) {
+            GElog ( icntr, "--- Number of differentibles do not match");
+            rc = 2;
+        }
+        else {
+            GElog ( icntr, "--- Model has the correct size.");
+            // icntr[I_Debug] = 1;
+            icntr[I_ConstDeriv] = 1; // signal that we will do constant derivatives
+        }
+        return rc;
+    }
+    else if ( icntr[I_Mode] == DOTERM ) {
+        // Do your cleanup here
+        GElog ( icntr, "--- Terminating");
+        return rc;
+    }
+    else if ( icntr[I_Mode] == DOEVAL ) {
+        GElog ( icntr, "--- Evaluation mode");
+        findex = icntr[I_Eqno];
+        dofnc = icntr[I_Dofunc];
+        dodrv = icntr[I_Dodrv];
+
+        // I_Newpt can save you computation in some circumstances
+        /*
+        if (icntr[I_Newpt] == 1) {
+            GElog ( icntr, "--- New point provided");
+        }else {
+            GElog ( icntr, "--- Old point again");
+        }
+        */
+
+        if ( findex >= 1 && findex <= 2 ) {
+            if ( dofnc ) {
+                if (findex == 1)
+                    *f = std::sin(x[0]) - x[2];
+                else
+                    *f = std::cos(x[1]) - x[3];
+            }
+            /*
+               The vector of derivatives is needed. The derivative with respect
+               to variable x(i) must be returned in d(i). The derivatives of the
+               linear terms, here -Z, must be defined each time.
+               */
+            if ( dodrv ) {
+                if(findex == 1)
+                {
+                    d[0] = std::cos(x[0]);
+                    d[2] = -1.0;
+                }
+                else {
+                    d[1] = -std::sin(x[1]);
+                    d[3] = -1;
+                }
+            }
+
+        }
+        else {
+            GEstat( icntr," ** fIndex has unexpected value.");
+            rc = 2;
+        }
+        return rc;
+    }
+    else if ( icntr[I_Mode] == DOCONSTDERIV ) {
+        GElog ( icntr, "--- Constant derivative call");
+        findex = icntr[I_Eqno];
+        d[findex + 1] = -1.0;
+        rc = 0;
+        return rc;
+    }
+    else {
+        GElog( icntr," ** Mode not defined.");
+        GEstat( icntr," ** Mode not defined.");
+        rc = 2;
+        return rc;
+    }
+}
---
+++ tests/integration/test_external_module.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import math
+import os
+import pathlib
+import platform
+import sys
+import unittest
+
+import gamspy as gp
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv(os.getcwd() + os.sep + ".env")
+except Exception:
+    pass
+
+
+class ExternalModuleTestSuite(unittest.TestCase):
+    def setUp(self):
+        self.m = gp.Container(
+            system_directory=os.getenv("GAMSPY_GAMS_SYSDIR", None)
+        )
+
+        directory = str(pathlib.Path(__file__).parent.resolve())
+        external_module = os.path.join(
+            directory, "external_module", "build", "libsimple_ext_module"
+        )
+
+        self.external_module = None
+        if platform.system() == "Linux":
+            external_module += ".so"
+            self.external_module = external_module
+
+    def test_sin_cos_example(self):
+        if self.external_module is None:
+            print("Not implemented for this platform yet")
+            return
+
+        y1 = gp.Variable(self.m, "y1")
+        y2 = gp.Variable(self.m, "y2")
+        x1 = gp.Variable(self.m, "x1")
+        x2 = gp.Variable(self.m, "x2")
+
+        eq1 = gp.Equation(self.m, "eq1", type="external")
+        eq2 = gp.Equation(self.m, "eq2", type="external")
+
+        eq1[...] = 1 * x1 + 3 * y1 == 1
+        eq2[...] = 2 * x2 + 4 * y2 == 2
+
+        model = gp.Model(
+            container=self.m,
+            name="sincos",
+            equations=self.m.getEquations(),
+            problem="NLP",
+            sense="min",
+            objective=y1 + y2,
+            external_module=self.external_module,
+        )
+
+        model.solve(output=sys.stdout, solver="conopt")
+
+        assert math.isclose(y1.toDense(), -1)
+        assert math.isclose(y2.toDense(), -1)
+
+
+def external_module_suite():
+    suite = unittest.TestSuite()
+    tests = [
+        ExternalModuleTestSuite(name)
+        for name in dir(ExternalModuleTestSuite)
+        if name.startswith("test_")
+    ]
+    suite.addTests(tests)
+
+    return suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(external_module_suite())
