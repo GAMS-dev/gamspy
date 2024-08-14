f2ac76de6c7c5cc1477ca0a96e231415edbd27e6
mabdullahsoyturk <msoyturk@gams.com>
2024-08-14 17:17:01 +0200

    add release notes

--- tests/test_gamspy.py
+++ tests/test_gamspy.py
@@ -49,7 +49,7 @@ class GamspySuite(unittest.TestCase):
     def test_version(self):
         import gamspy
 
-        self.assertEqual(gamspy.__version__, "0.14.4")
+        self.assertEqual(gamspy.__version__, "0.14.5")
 
 
 class DocsSuite(unittest.TestCase):
