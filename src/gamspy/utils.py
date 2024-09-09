5d9d0e1df2060a43d3f89040f7c2fbfb584bb47b
mabdullahsoyturk <msoyturk@gams.com>
2024-09-09 15:44:10 +0200

    change the order of looking at directories

--- src/gamspy/utils.py
+++ src/gamspy/utils.py
@@ -320,16 +320,16 @@ def _get_gamspy_base_directory() -> str:
 
 
 def _get_license_path(system_directory: str) -> str:
+    # Check if a new license was installed.
+    gamspy_license_path = os.path.join(DEFAULT_DIR, "gamspy_license.txt")
+    if os.path.exists(gamspy_license_path):
+        return gamspy_license_path
+
     # Check old license installation path.
     # TODO: Remove this after 1.0.1 release.
     user_license_path = os.path.join(system_directory, "user_license.txt")
     if os.path.exists(user_license_path):
         return user_license_path
-
-    # Check if a new license was installed.
-    gamspy_license_path = os.path.join(DEFAULT_DIR, "gamspy_license.txt")
-    if os.path.exists(gamspy_license_path):
-        return gamspy_license_path
 
     # No preinstalled licenses on the machine. Use the demo license.
     return os.path.join(system_directory, "gamslice.txt")
