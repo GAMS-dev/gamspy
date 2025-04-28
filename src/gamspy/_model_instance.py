2143156a4479bc0e23ea496602b0bc66eebe7b86
mabdullahsoyturk <msoyturk@gams.com>
2025-04-28 12:37:55 +0200

    shorten scenario name

--- src/gamspy/_model_instance.py
+++ src/gamspy/_model_instance.py
@@ -556,7 +556,7 @@ class ModelInstance:
         return self.summary
 
     def _get_scenario(self, model: Model) -> str:
-        auto_id = "s" + utils._get_unique_name().split("-")[0]
+        auto_id = "s" + utils._get_unique_name()[:4]
         params = [
             modifier.gams_symbol
             for modifier in self.modifiers
