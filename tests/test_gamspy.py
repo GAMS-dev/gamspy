15fd848e34961528d7bfa84eebf7581e82c652f8
mabdullahsoyturk <msoyturk@gams.com>
2026-08-03 10:14:32 +0200

    add release notes

--- tests/test_gamspy.py
+++ tests/test_gamspy.py
@@ -16,7 +16,7 @@ from gamspy.exceptions import GamspyException, ValidationError
 
 @pytest.mark.unit
 def test_version():
-    assert gp.__version__ == "1.26.2"
+    assert gp.__version__ == "1.26.3"
 
 
 @pytest.mark.unit
