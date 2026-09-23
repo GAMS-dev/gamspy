e7d738303ea385132c467273a0bd4074815174f2
mabdullahsoyturk <msoyturk@gams.com>
2026-09-23 17:17:34 +0200

    [chore] use ci project dir for the license instead of gamspy_base

--- src/gamspy/utils.py
+++ src/gamspy/utils.py
@@ -579,11 +579,6 @@ def _get_license_path(system_directory: str) -> str:
 
         return license_path
 
-    # Check ci license
-    ci_license_path = os.path.join(system_directory, "ci_license.txt")
-    if os.path.exists(ci_license_path):
-        return ci_license_path
-
     # Check if a new license was installed.
     gamspy_license_path = os.path.join(DEFAULT_DIR, "gamspy_license.txt")
     if os.path.exists(gamspy_license_path):
